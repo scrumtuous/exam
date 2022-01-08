94b4d19c70fa3ed95daf21d9c4d1ee84d4f80d6c
darcy <darcy@example.com>
2022-01-08 16:27:13 -0500

    stuff

--- src/Components/QuestionJumperPanel.js
+++ src/Components/QuestionJumperPanel.js
@@ -11,15 +11,13 @@ class QuestionJumperPanel extends Component{
     render(){
         return (
 
-			<div class="card mt-2">
-			   <div class="card-header d-flex justify-content-between align-items-center" id="questionJumperTitle" onClick={() => this.toggleWindow()}>
+			<div class="card mt-3 mb-3 qj">
+			   <div class="card-header d-flex justify-content-between align-items-center" id="questionJumperTitle" onClick={() => this.formatToTable()}>
 
   Question Jumper
-<svg id="caret" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-up" viewBox="0 0 16 16">
-<path d="M3.204 5h9.592L8 10.481 3.204 5zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z"/>
-</svg>
-
-
+<button type="button" class="close" aria-label="Close">
+  <span aria-hidden="true"><small>_</small></span>
+</button>
 			  
 
 
@@ -30,16 +28,7 @@ class QuestionJumperPanel extends Component{
 				  <div id="questionJumper">
 				  {
 					  this.props.questions.map((value, index) => { 
-						return <JumperButton 
-						indexNumber={index} 
-						setCurrentQuestion={this.props.setCurrentQuestion} 
-						questions={this.props.questions}
-						question={this.props.questions[index]}
-						currentQuestionNumber = {this.props.currentQuestionNumber} 
-						cheating={this.props.cheating}
-						graded={this.props.graded}
-						toggleMarked={this.props.toggleMarked}
-						></JumperButton>
+						return <JumperButton indexNumber={index} setCurrentQuestion={this.props.setCurrentQuestion} ></JumperButton>
 					  }
 					)
 				  }
@@ -50,19 +39,16 @@ class QuestionJumperPanel extends Component{
         )
     }
 
-	toggleWindow() {
+	formatToTable() {
 
 	
 var x = document.getElementById("jumper-card-body");
-var y = document.getElementById("caret");
+
 
   if (x.style.display === "none") {
     x.style.display = "block";
-	y.innerHTML = '<path d="M3.204 5h9.592L8 10.481 3.204 5zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z"/>';
   } else {
     x.style.display = "none";
-	y.innerHTML = '<path d="M3.204 11h9.592L8 5.519 3.204 11zm-.753-.659 4.796-5.48a1 1 0 0 1 1.506 0l4.796 5.48c.566.647.106 1.659-.753 1.659H3.204a1 1 0 0 1-.753-1.659z"/>';
-
   }
 } 
 
